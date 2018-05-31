d5abe9812758970e6ebc7a2aa04b137f973ae711
gabor <GaborZay@gmail.com>
2018-05-30 21:34:28 -0700

    Added in Authentication with firebase.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,9 @@
+import firebase from 'firebase';
 import {
     EMAIL_CHANGED,
-    PASSWORD_CHANGED
+    PASSWORD_CHANGED,
+    LOGIN_USER_SUCCESS,
+    LOGIN_USER_FAIL
 } from './types';
 
 export const emailChanged = (text) => {
@@ -15,4 +18,32 @@ export const passwordChanged = (text) => {
         type: PASSWORD_CHANGED,
         payload: text
     };
+};
+
+export const loginUser = ({email, password}) => {
+    return (dispatch) => {
+        firebase.auth()
+            .signInWithEmailAndPassword(email, password)
+            .then(user => loginUserSuccess(dispatch, user))
+            .catch(error => {
+                console.log(error);
+                firebase.auth().createUserWithEmailAndPassword(email, password)
+                    .then(user => loginUserSuccess(dispatch, user))
+                    .catch(error => loginUserFail(dispatch, error))
+            });
+    };
+};
+
+const loginUserSuccess = (dispatch, user) => {
+    dispatch({
+        type: LOGIN_USER_SUCCESS,
+        payload: user
+    });
+};
+
+const loginUserFail = (dispatch, error) => {
+    dispatch({
+        type: LOGIN_USER_FAIL,
+        payload: error
+    });
 };
